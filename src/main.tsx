5b9b0af9972c27dd3f7a47037c833e2448c51254
maciejlichon <maciej.lichon@geckodynamics.com>
2023-10-25 15:40:37 +0200

    publish

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={'/FM_Scoot-multi-page-website/'}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
