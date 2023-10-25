25d17d3c9356448b49a94af9d4c76fcff8258cbd
maciejlichon <maciej.lichon@geckodynamics.com>
2023-10-25 15:51:01 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
-  base: "/FM_Scoot-multi-page-website/",
+  base: '/FM_Scoot-multi-page-website/',
 });
